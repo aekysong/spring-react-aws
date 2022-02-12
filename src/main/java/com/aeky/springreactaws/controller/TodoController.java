f158c0fe6b775291688b36926f3173e3a111bc6e
yaekyoon.song <yaekyoon.song@kt.com>
2022-02-12 21:34:15 +0900

    Todo CRUD

--- src/main/java/com/aeky/springreactaws/controller/TodoController.java
+++ src/main/java/com/aeky/springreactaws/controller/TodoController.java
@@ -1,15 +1,17 @@
 package com.aeky.springreactaws.controller;
 
 import com.aeky.springreactaws.dto.ResponseDto;
+import com.aeky.springreactaws.dto.TodoDto;
+import com.aeky.springreactaws.model.TodoEntity;
 import com.aeky.springreactaws.service.TodoService;
+import com.sun.tools.javac.comp.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -25,5 +27,74 @@ public class TodoController {
         list.add(str);
         ResponseDto<String> response = ResponseDto.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodo() {
+        String tempUserId = "temp-user";
+
+        List<TodoEntity> entities = todoService.retrieve(tempUserId);
+        List<TodoDto> dtos = entities.stream().map(TodoDto::new).collect(Collectors.toList());
+        ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().data(dtos).build();
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDto dto) {
+        try {
+            String tempUserId = "temp-user";
+
+            // DTO --> 엔티티
+            TodoEntity entity = TodoDto.toEntity(dto);
+            entity.setId(null); // id 초기화
+            entity.setUserId(tempUserId);
+
+            // 엔티티 저장
+            List<TodoEntity> entities = todoService.create(entity);
+
+            // 엔티티 --> DTO
+            // 응답 생성
+            List<TodoDto> dtos = entities.stream().map(TodoDto::new).collect(Collectors.toList());
+            ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().data(dtos).build();
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateTodo(@RequestBody TodoDto dto) {
+        String tempUserId = "temp-user";
+
+        TodoEntity entity = TodoDto.toEntity(dto);
+        entity.setUserId(tempUserId);
+
+        List<TodoEntity> entities = todoService.update(entity);
+        List<TodoDto> dtos = entities.stream().map(TodoDto::new).collect(Collectors.toList());
+
+        ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().data(dtos).build();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteTodo(@RequestBody TodoDto dto) {
+        try {
+            String tempUserId = "temp-user";
+            TodoEntity entity = TodoDto.toEntity(dto);
+            entity.setUserId(tempUserId);
+
+            List<TodoEntity> entities = todoService.delete(entity);
+            List<TodoDto> dtos = entities.stream().map(TodoDto::new).collect(Collectors.toList());
+
+            ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().data(dtos).build();
+            return ResponseEntity.ok().body(response);
+        } catch(Exception e) {
+            String error = e.getMessage();
+            ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
--- src/main/java/com/aeky/springreactaws/service/TodoService.java
+++ src/main/java/com/aeky/springreactaws/service/TodoService.java
@@ -2,9 +2,15 @@ package com.aeky.springreactaws.service;
 
 import com.aeky.springreactaws.model.TodoEntity;
 import com.aeky.springreactaws.persistence.TodoRepository;
+import com.sun.tools.javac.comp.Todo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j  // 로깅
 @Service
 public class TodoService {
 
@@ -16,5 +22,61 @@ public class TodoService {
         todoRepository.save(entity);
         TodoEntity savedEntity = todoRepository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        // 엔티티 유효성 검증
+        validate(entity);
+
+        // 엔티티 DB 저장
+        todoRepository.save(entity);
+        log.info("Entity ID: {} is saved.", entity.getId());
+
+        // 저장된 엔티티를 포함해 리스트 리턴
+        return todoRepository.findByUserId(entity.getUserId());
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return todoRepository.findByUserId(userId);
+    }
+
+    public List<TodoEntity> update(final TodoEntity entity) {
+        validate(entity);
+
+        final Optional<TodoEntity> original = todoRepository.findById(entity.getId());
+        original.ifPresent(todo -> {
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            todoRepository.save(todo);
+            log.info("Entity ID: {} is modified.", entity.getId());
+        });
+
+        return retrieve(entity.getUserId());
+    }
+
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        validate(entity);
+
+        try {
+            todoRepository.delete(entity);
+        } catch(Exception e) {
+            log.error("error deleting entity ", entity.getId(), e);
+            throw new RuntimeException("error deleting entity " + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
     }
 }
